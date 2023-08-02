1d9c62285ef702eac999061316f5493e2abd4593
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-08-02 20:04:07 +0600

    Navbar complete

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,9 +1,47 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import AddTask from "../Pages/AddTask/AddTask";
+import AllTask from "../Pages/AllTask/AllTask";
+import MyTask from "../Pages/MyTask/MyTask";
+import Signin from "../Pages/Signin/Signin";
+import Signup from "../Pages/Signup/Signup";
+import Contact from "../Pages/Contact/Contact";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/addtask',
+                element: <AddTask></AddTask>
+            },
+            {
+                path: '/alltask',
+                element: <AllTask></AllTask>
+            },
+            {
+                path: '/mytask',
+                element: <MyTask></MyTask>
+            },
+            {
+                path: '/contact',
+                element: <Contact></Contact>
+            },
+            {
+                path: '/signin',
+                element: <Signin></Signin>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            }
+        ]
     },
 ]);
 
